9d54524023ab9ca74d6748cf3dbf81c5900d7642
User <delvan>
2025-08-05 21:52:50 -0300

    Criando Projeto inicial Delivery

---
+++ Microservices/Courier-Management/src/main/java/com/ideaprog/ideafood/courier/management/CourierManagementApplication.java
@@ -0,0 +1,13 @@
+package com.ideaprog.ideafood.courier.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourierManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourierManagementApplication.class, args);
+	}
+
+}
